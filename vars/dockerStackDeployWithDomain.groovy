1eeaacf1dd3e94b5468149f197568d35b9d10ec4
Patrick Scott <patrick.scott@zx-ventures.com>
2017-11-01 22:41:58 -0400

    use ENV from outer level

--- vars/dockerStackDeployWithDomain.groovy
+++ vars/dockerStackDeployWithDomain.groovy
@@ -1,13 +1,9 @@
 def call(String stackName, String domain) {
   script {
-    withEnv([
-      "SERVICE_DOMAIN=${domain}"
-    ]) {
-      if (domain) {
-        sh "docker stack deploy --prune -c stack.yml ${stackName}"
-      } else {
-        sh 'echo "ERROR: domain is required." && exit 1'
-      }
+    if (domain) {
+      sh "docker stack deploy --prune -c stack.yml ${stackName}"
+    } else {
+      sh 'echo "ERROR: domain is required." && exit 1'
     }
   }
 }
