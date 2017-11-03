a912994f05a9b39a1a2ebab5a431e376bbd16fcc
Patrick Scott <patrick.scott@zx-ventures.com>
2017-11-03 11:07:16 -0400

    assume built with reg name

--- vars/dockerTagAndPush.groovy
+++ vars/dockerTagAndPush.groovy
@@ -1,6 +1,6 @@
 def call(String registry, String imageName, String tag) {
   script {
-    sh "docker tag ${imageName} \
+    sh "docker tag ${registry}/${imageName} \
       ${registry}/${imageName}:${tag}"
     sh "docker push \
       ${registry}/${imageName}:${tag}"
