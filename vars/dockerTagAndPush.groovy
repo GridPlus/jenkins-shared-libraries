527cc98e498c49ea20067631bd4a3fb89b07d853
Patrick Scott <patrick.scott@zx-ventures.com>
2018-02-13 11:30:50 -0500

    consolidate jenkins shared libs

--- vars/dockerTagAndPush.groovy
+++
@@ -1,8 +0,0 @@
-def call(String registry, String imageName, String tag) {
-  script {
-    sh "docker tag ${registry}/${imageName} \
-      ${registry}/${imageName}:${tag}"
-    sh "docker push \
-      ${registry}/${imageName}:${tag}"
-  }
-}
