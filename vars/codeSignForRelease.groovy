53efd4ef94015db51db75745b8029a3347076918
Brandon Mayes <brandon.mayes@gridplus.io>
2018-05-14 17:39:50 -0500

    Update to (hopefully) a fully-implemented version

--- vars/codeSignForRelease.groovy
+++ vars/codeSignForRelease.groovy
@@ -1,15 +1,19 @@
-def call() {
+def call(outputFile) {
     withCredentials([string(
             credentialsId: "code-signing-key-pw",
             variable: "GPG_PASS"
     )]) {
+        sh (
+            script: 'expect -c "spawn gpg --import code_signing_key; send \"${GPG_PASS}\"; expect eof"'
+        )
+
         IMPORTED_KEY_ID = sh (
-            script: 'expect -c "spawn gpg --import-keys code_signing_key | head -n 2 | tail -n 1; send \"${GPG_PASS}\"; expect eof"',
+            script: 'gpg --list-keys code_signing_key | head -n 2 | tail -n 1 | awk \'{print $1}\'',
             returnStdOut: true
         ).trim()
 
         sh (
-            script: 'expect -c "spawn gpg --edit-key ${IMPORTED_KEY_ID} trust quit; send \"5\ry\r\"; expect eof"'
+            script: 'expect -c "spawn gpg -u ${IMPORTED_KEY_ID} --sign ${outputFile}; send \"y\"; expect eof"'
         )
     }
 }
