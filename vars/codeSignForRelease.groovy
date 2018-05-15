e02606a086f61dabb39a376401365e6b322cd6e4
Brandon Mayes <brandon.mayes@gridplus.io>
2018-05-15 11:26:55 -0500

    Try adding --batch option to workaround tty error

--- vars/codeSignForRelease.groovy
+++ vars/codeSignForRelease.groovy
@@ -6,16 +6,16 @@ def call(outputFile) {
         sh "export GPG_TTY=`tty`"
 
         sh (
-            script: 'expect -c "spawn gpg --import /run/secrets/code_signing_key; send \"${GPG_PASS}\"; expect eof"'
+            script: 'expect -c "spawn gpg --batch --import /run/secrets/code_signing_key; send \"${GPG_PASS}\"; expect eof"'
         )
 
         IMPORTED_KEY_ID = sh (
-            script: 'gpg --list-keys code_signing_key | head -n 2 | tail -n 1 | awk \'{print $1}\'',
+            script: 'gpg --batch --list-keys code_signing_key | head -n 2 | tail -n 1 | awk \'{print $1}\'',
             returnStdOut: true
         ).trim()
 
         sh (
-            script: 'expect -c "spawn gpg -u ${IMPORTED_KEY_ID} --sign ${outputFile}; send \"y\"; expect eof"'
+            script: 'expect -c "spawn gpg --batch -u ${IMPORTED_KEY_ID} --sign ${outputFile}; send \"y\"; expect eof"'
         )
     }
 }
