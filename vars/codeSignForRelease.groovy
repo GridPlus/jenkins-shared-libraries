33f9d9dbb88d96211952697e416a48392bcaff0f
Brandon Mayes <brandon.mayes@gridplus.io>
2018-05-15 11:30:47 -0500

    Remove trim function to see if output is properly saved to env. var.

--- vars/codeSignForRelease.groovy
+++ vars/codeSignForRelease.groovy
@@ -3,16 +3,14 @@ def call(outputFile) {
             credentialsId: "code-signing-key-pw",
             variable: "GPG_PASS"
     )]) {
-        sh "export GPG_TTY=`tty`"
-
         sh (
             script: 'expect -c "spawn gpg --batch --import /run/secrets/code_signing_key; send \"${GPG_PASS}\"; expect eof"'
         )
 
         IMPORTED_KEY_ID = sh (
-            script: 'gpg --batch --list-keys code_signing_key | head -n 2 | tail -n 1 | awk \'{print $1}\'',
+            script: 'gpg --list-keys code_signing_key | head -n 2 | tail -n 1 | awk \'{print $1}\'',
             returnStdOut: true
-        ).trim()
+        )
 
         sh (
             script: 'expect -c "spawn gpg --batch -u ${IMPORTED_KEY_ID} --sign ${outputFile}; send \"y\"; expect eof"'
