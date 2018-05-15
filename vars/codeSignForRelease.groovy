b47c79117aea549d438416cf2ec8b5792a492109
Brandon Mayes <brandon.mayes@gridplus.io>
2018-05-15 13:37:54 -0500

    Really this time, we're adding the --yes option

--- vars/codeSignForRelease.groovy
+++ vars/codeSignForRelease.groovy
@@ -13,7 +13,7 @@ def call(outputFile) {
         ).trim()
 
         sh (
-            script: "expect -c \"spawn gpg --batch -u ${IMPORTED_KEY_ID} --sign ${outputFile}; send \"${GPG_PASS}\"; expect eof\""
+            script: "expect -c \"spawn gpg --batch --yes -u ${IMPORTED_KEY_ID} --sign ${outputFile}; send \"${GPG_PASS}\"; expect eof\""
         )
     }
 }
