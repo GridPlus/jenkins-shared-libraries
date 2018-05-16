16c96e9443f4248a1e42efe63ab4c4e5b8b9f1a8
Brandon Mayes <brandon.mayes@gridplus.io>
2018-05-16 14:13:42 -0500

    Put single quotes around the expect command

--- vars/codeSignForRelease.groovy
+++ vars/codeSignForRelease.groovy
@@ -3,6 +3,6 @@ def call(outputFile) {
             credentialsId: "code-signing-key-pw",
             variable: "MINISIGN_PASS"
     )]) {
-        sh """expect -c \\"spawn minisign -Sm ${outputFile} -s /run/secrets/code_signing_key; sleep 2; send \\"${MINISIGN_PASS}\\r\\"; expect eof\\""""
+        sh """expect -c 'spawn minisign -Sm ${outputFile} -s /run/secrets/code_signing_key; sleep 2; send \\"${MINISIGN_PASS}\\r\\"; expect eof'"""
     }
 }
