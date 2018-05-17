03c89181ab03db729765473e99070ee1462d326f
Brandon Mayes <brandon.mayes@gridplus.io>
2018-05-17 15:50:02 -0500

    Remove pipeline as those cannot be nested. Not sure if a nested stage works but we may ultimately not need this anyway. Leaving as-is for now.

--- vars/dockerRelease.groovy
+++ vars/dockerRelease.groovy
@@ -1,21 +1,16 @@
 def call(String registry, String container, String tag) {
   def latest = env.BRANCH_NAME == 'staging' ? 'latest-staging' : 'latest';
 
-  pipeline {
-    agent "build"
-    stages {
-      stage('docker-release') {
-        when {
-          expression { BRANCH_NAME ==~ /(master|staging)/ }
-          beforeAgent true
-        }
-        steps {
-          dockerLogin()
-          dockerTagAndPushImage(registry, container, latest)
-          dockerTagAndPushImage(registry, container, tag)
-          dockerLogout()
-        }
-      }
+  stage('docker-release') {
+    when {
+      expression { env.BRANCH_NAME ==~ /(master|staging)/ }
+      beforeAgent true
+    }
+    steps {
+      dockerLogin()
+      dockerTagAndPushImage(registry, container, latest)
+      dockerTagAndPushImage(registry, container, tag)
+      dockerLogout()
     }
   }
 }
