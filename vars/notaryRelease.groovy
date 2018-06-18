d9d2653bd9a54dba5d643abb9ab509111d9d09df
Daniel Veenstra <dan.veenstra@gridplus.io>
2018-06-18 16:51:59 -0500

    Adding compression of files in release.txt

--- vars/notaryRelease.groovy
+++ vars/notaryRelease.groovy
@@ -1,4 +1,4 @@
-def call(repoName, releaseManifest, targetName) {
+def call(repoName, targetName, releaseManifest = 'release.txt') {
     withCredentials([
           usernamePassword(credentialsId: "nexus-upload-only",
             usernameVariable: "NEXUS_UPLOAD_USER",
@@ -7,13 +7,13 @@ def call(repoName, releaseManifest, targetName) {
           string(credentialsId: "${repoName}-targets-key-pw", variable: "NOTARY_TARGETS_PASSPHRASE")
             ]) 
             {
-              //TODO: Do some stuff here to zip up the files defined in the releaseManifest, and assign archive to targetFile
-              def targetFile = releaseManifest
+              
+              sh "tar -czf target.tgz -T ${releaseManifest}"
               //Move targets key to trust dir
               sh "mkdir -p trust_dir/private/"
               sh "mv $TARGETS_KEY trust_dir/private/"
 
-              sh "curl --user ${NEXUS_UPLOAD_USER}:${NEXUS_UPLOAD_PASS} --upload-file ${targetFile} https://nexus.staging-gridpl.us/repository/agent/${repoName}/${targetName}"
-              sh "notary -s https://notary.staging-gridpl.us -d ./trust_dir add -p ${repoName} ${targetName} ${targetFile}"
+              sh "curl --user ${NEXUS_UPLOAD_USER}:${NEXUS_UPLOAD_PASS} --upload-file target.tgz https://nexus.staging-gridpl.us/repository/agent/${repoName}/${targetName}"
+              sh "notary -s https://notary.staging-gridpl.us -d ./trust_dir add -p ${repoName} ${targetName} target.tgz"
             }
 }
