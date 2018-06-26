aa2b05cd0f965da65239759d9a9a2b9b7d4fdc6f
Daniel Veenstra <dan.veenstra@gridplus.io>
2018-06-26 14:38:28 -0500

    Adding manual docker login to nexus staging for testing purposes

--- vars/dockerSignedRelease.groovy
+++ vars/dockerSignedRelease.groovy
@@ -4,13 +4,17 @@ def call(String orgName, String imageName, String tag) {
   script {
     withCredentials([
       file(credentialsId: "${imageURI}-targets-key", variable: "TARGETS_KEY"),
-      string(credentialsId: "${imageURI}-targets-key-pw", variable: "DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE")
+      string(credentialsId: "${imageURI}-targets-key-pw", variable: "DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE"),
+      usernamePassword(
+            credentialsId: "docker",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
     ]) {
       sh "export DOCKER_CONTENT_TRUST=1"
       sh "export DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
       sh "mkdir -p ~/.docker/trust/private/"
       sh "mv $TARGETS_KEY ~/.docker/trust/private/"
-      dockerLogin()
+      sh "docker login ${dockerRegistry} -u ${USER} -p ${PASS}"
       sh "docker tag ${imageName} ${imageURI}:${tag}"
       sh "docker push ${imageURI}:${tag}"
       dockerLogout()
