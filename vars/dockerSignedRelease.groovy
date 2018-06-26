c39d3931f4837f39ae5bf44767929d1e15f17cf3
Daniel Veenstra <dan.veenstra@gridplus.io>
2018-06-26 15:22:25 -0500

    cleanign up entire docker private key dir after run

--- vars/dockerSignedRelease.groovy
+++ vars/dockerSignedRelease.groovy
@@ -15,15 +15,12 @@ def call(String orgName, String imageName, String tag) {
         "DOCKER_CONTENT_TRUST=1",
         "DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
       ]) {
-        // sh "export DOCKER_CONTENT_TRUST=1"
-        // sh "export DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
         sh "mkdir -p ~/.docker/trust/private/"
         sh "mv $TARGETS_KEY ~/.docker/trust/private/"
         sh "docker login ${dockerRegistry} -u ${USER} -p ${PASS}"
         sh "docker tag ${imageName} ${imageURI}:${tag}"
         sh "docker push ${imageURI}:${tag}"
-        sh "ls ~/.docker/trust/private/"
-        sh "rm ~/.docker/trust/private/${TARGETS_KEY}"
+        sh "rm -rf ~/.docker/trust/private/"
         dockerLogout()
       }
     } 
