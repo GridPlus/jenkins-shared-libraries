72a354d9ed0ce66b64c982903a2e10f245816081
Daniel Veenstra <dan.veenstra@gridplus.io>
2018-06-26 14:49:53 -0500

    Trying different way to set env variables

--- vars/dockerSignedRelease.groovy
+++ vars/dockerSignedRelease.groovy
@@ -1,6 +1,10 @@
 def call(String orgName, String imageName, String tag) {
   dockerRegistry = 'docker.staging-gridpl.us'
   imageURI = "${dockerRegistry}/${orgName}/${imageName}"
+  environment {
+    DOCKER_CONTENT_TRUST="1"
+    DOCKER_CONTENT_TRUST_SERVER="https://notary.staging-gridpl.us"
+  }
   script {
     withCredentials([
       file(credentialsId: "${imageURI}-targets-key", variable: "TARGETS_KEY"),
@@ -10,8 +14,8 @@ def call(String orgName, String imageName, String tag) {
             usernameVariable: "USER",
             passwordVariable: "PASS")
     ]) {
-      sh "export DOCKER_CONTENT_TRUST=1"
-      sh "export DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
+      // sh "export DOCKER_CONTENT_TRUST=1"
+      // sh "export DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
       sh "mkdir -p ~/.docker/trust/private/"
       sh "mv $TARGETS_KEY ~/.docker/trust/private/"
       sh "docker login ${dockerRegistry} -u ${USER} -p ${PASS}"
