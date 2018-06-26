6514814a9086a021140c66d59889f39d6a25b3bd
Daniel Veenstra <dan.veenstra@gridplus.io>
2018-06-26 15:00:03 -0500

    New way to set env variables

--- vars/dockerSignedRelease.groovy
+++ vars/dockerSignedRelease.groovy
@@ -1,10 +1,7 @@
 def call(String orgName, String imageName, String tag) {
   dockerRegistry = 'docker.staging-gridpl.us'
   imageURI = "${dockerRegistry}/${orgName}/${imageName}"
-  environment {
-    DOCKER_CONTENT_TRUST="1"
-    DOCKER_CONTENT_TRUST_SERVER="https://notary.staging-gridpl.us"
-  }
+  
   script {
     withCredentials([
       file(credentialsId: "${imageURI}-targets-key", variable: "TARGETS_KEY"),
@@ -14,15 +11,20 @@ def call(String orgName, String imageName, String tag) {
             usernameVariable: "USER",
             passwordVariable: "PASS")
     ]) {
-      // sh "export DOCKER_CONTENT_TRUST=1"
-      // sh "export DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
-      sh "mkdir -p ~/.docker/trust/private/"
-      sh "mv $TARGETS_KEY ~/.docker/trust/private/"
-      sh "docker login ${dockerRegistry} -u ${USER} -p ${PASS}"
-      sh "docker tag ${imageName} ${imageURI}:${tag}"
-      sh "printenv"
-      sh "docker push ${imageURI}:${tag}"
-      dockerLogout()
+      withEnv([
+        "DOCKER_CONTENT_TRUST=1",
+        "DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
+      ]) {
+        // sh "export DOCKER_CONTENT_TRUST=1"
+        // sh "export DOCKER_CONTENT_TRUST_SERVER=https://notary.staging-gridpl.us"
+        sh "mkdir -p ~/.docker/trust/private/"
+        sh "mv $TARGETS_KEY ~/.docker/trust/private/"
+        sh "docker login ${dockerRegistry} -u ${USER} -p ${PASS}"
+        sh "docker tag ${imageName} ${imageURI}:${tag}"
+        sh "printenv"
+        sh "docker push ${imageURI}:${tag}"
+        dockerLogout()
+      }
     } 
   }
 }
