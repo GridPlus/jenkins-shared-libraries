d2bae04d944c8e2768cacbaafad9ef1acb5dbd40
Daniel Veenstra <dan.veenstra@gridplus.io>
2018-07-16 14:20:57 -0500

    DRY'ed up nexusDockerRelease to use nexusDockerLogin and nexusDockerTagAndPushImage

--- vars/nexusDockerRelease.groovy
+++ vars/nexusDockerRelease.groovy
@@ -3,17 +3,8 @@ def call(String repoName, String imageName, String tag) {
   imageURI = "${registry}/${repoName}/${imageName}"
 
   script {
-    withCredentials([
-      usernamePassword(
-            credentialsId: "jenkins-docker",
-            usernameVariable: "USER",
-            passwordVariable: "PASS")
-    ]) {
-        sh "docker login ${registry} -u ${USER} -p ${PASS}"
-        sh "docker tag ${imageName} ${imageURI}:${tag}"
-        sh "docker push ${imageURI}:${tag}"
-        dockerLogout()
-      }
-    }
+    nexusDockerLogin()
+    nexusDockerTagAndPushImage(repoName,imageName,tag)
+    dockerLogout()
   }
 }
--- vars/nexusDockerTagAndPushImage.groovy
+++ vars/nexusDockerTagAndPushImage.groovy
@@ -1,4 +1,4 @@
-def call(String registry, String imageName, String tag) {
+def call(String repoName, String imageName, String tag) {
   registry = 'docker.gridpl.us'
   imageURI = "${registry}/${repoName}/${imageName}"
 
