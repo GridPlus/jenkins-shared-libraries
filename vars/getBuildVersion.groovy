f44ab883df1cc94e408ec315733b059cd6eb1f65
Matt Walters <matt@gridplus.io>
2018-10-04 11:22:39 -0500

    ensuring we dont have branch collisions by appending non-master branch names to build version

--- vars/getBuildVersion.groovy
+++ vars/getBuildVersion.groovy
@@ -1,6 +1,10 @@
 import java.text.SimpleDateFormat
 def call() {
   def dateFormat = new SimpleDateFormat("yyyy.MM.dd")
-  currentBuild.displayName = dateFormat.format(new Date()) + "-" + env.BUILD_NUMBER
+  def buildNumber = env.BUILD_NUMBER;
+  if (env.BRANCH_NAME != 'master') {
+    buildNumber = buildNumber + '-' + env.BRANCH_NAME
+  }
+  currentBuild.displayName = dateFormat.format(new Date()) + "-" + buildNumber
   return currentBuild.displayName
 }
