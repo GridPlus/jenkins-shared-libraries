a6c6b1b3bc1e8baed3a8fc8d2a25a3c23c264e6f
Brandon Mayes <brandon.mayes@gridplus.io>
2018-11-08 11:22:49 -0600

    Rollback branching changes as it seems to dislike the when command

--- vars/dockerRelease.groovy
+++ vars/dockerRelease.groovy
@@ -2,15 +2,9 @@ def call(String repoName, String imageName, String tag) {
   registry = 'docker.gridpl.us'
   imageURI = "${registry}/${repoName}/${imageName}"
 
-  stage('docker-release') {
-    when {
-      expression { env.BRANCH_NAME ==~ /(master|staging)/ }
-      beforeAgent true
-    }
-    steps {
-      dockerLogin()
-      dockerTagAndPushImage(repoName,imageName,tag)
-      dockerLogout()
-    }
+  script {
+    dockerLogin()
+    dockerTagAndPushImage(repoName,imageName,tag)
+    dockerLogout()
   }
 }
