d22ac03c321d6f41b9d9835a7b52c127626c35e2
Brandon Mayes <brandon.mayes@gridplus.io>
2018-11-08 11:00:10 -0600

    Escape slashes in clubhouse auto-generated branch names

--- vars/getBuildVersion.groovy
+++ vars/getBuildVersion.groovy
@@ -3,7 +3,7 @@ def call() {
   def dateFormat = new SimpleDateFormat("yyyy.MM.dd")
   def buildNumber = env.BUILD_NUMBER;
   if (env.BRANCH_NAME != 'master') {
-    buildNumber = buildNumber + '-' + env.BRANCH_NAME
+    buildNumber = buildNumber + '-' + env.BRANCH_NAME.replaceAll('/', '-');
   }
   currentBuild.displayName = dateFormat.format(new Date()) + "-" + buildNumber
   return currentBuild.displayName
